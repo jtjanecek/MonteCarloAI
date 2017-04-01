2f87735eda6f30c13265a24f32179232fc88e253
jtjanecek <janecektyler@gmail.com>
2017-03-31 17:30:04 -0700

    Game.py now contains only game logic for simple board game

--- Game.py
+++ Game.py
@@ -1,147 +1,37 @@
-
-
-''' Game object with 2 players. Player -1 and player 1. 0 represents non player.
-'''
-
+import AI
+from Checkers import Checkers
+import time
 # Abstract class
 class Game():
-    def __init__(self):
+    def __init__(self, game, player1, player2):
         ''' Initialize game board etc
         '''
-        pass
+        self.gameLoop(game, player1, player2)
 
-    def makeMove(self, player: int, move: tuple) -> bool:
-        '''
-        Parameters:
-            player: -1 or 1
-            move: tuple for board placement
-        Return:
-            boolean:
-                true - move success
-                false - move failed, move could not be placed
-        '''
-        pass
-
-    def isValidMove(self, board:[list], move: tuple) -> bool:
-        '''
-        Checks if current board and move are valid
-        '''
-        pass
-
-    def getAllPossibleMoves(self, currPlayer: int) -> list:
-        '''
-        Parameters:
-            currPlayer: -1 or 1
-        Return:
-            list of possible moves in tuple form
-        '''
-        pass
-
-    def hasWinner(self) -> int:
-        '''
-        Return:
-            boolean, -1 if player -1 is winner
-                      1 if player 1 is winner
-                      0 if no winner
-        '''
-        pass
+    def gameLoop(self, game, player1, player2):
+        game.printBoard()
+        self._currMove = -1
+        while(game.getWinner() == 0):
+            time.sleep(1)
+            if self._currMove == -1:
+                self._currMove = 1
+                flag = True
+                while flag:
+                    move = player1.getMove(list(game.getBoard()))
+                    if game.isValidMove(move, -1):
+                        game.makeMove(-1, move)
+                        flag = False;
+            elif self._currMove == 1:
+                self._currMove = -1
+                flag = True
+                while flag:
+                    move = player2.getMove(list(game.getBoard()))
+                    if game.isValidMove(move, 1):
+                        game.makeMove(1, move)
+                        flag = False;
+        print(game.getWinner()," is the winner!")
 
 
-
-    def makeRandomMove(self, currPlayer) -> bool:
-        '''
-        Parameters:
-            currPlayer: -1 or 1
-        Returns boolean:
-            true - move success
-            false - move failed
-        '''
-        pass
-
-    def printBoad(self) -> None:
-        '''
-        Prints board to console
-        '''
-
-
-
-
-
-
-'''
-    Checkers
-'''
-class Checkers(Game):
-    def __init__(self):
-        ''' Initialize game board etc
-        '''
-        # 8 by 8 board
-        self._board = [
-            [0,-1,0,-1,0,-1,0,-1],
-            [-1,0,-1,0,-1,0,-1,0],
-            [0,-1,0,-1,0,-1,0,-1],
-            [0,0,0,0,0,0,0,0,],
-            [0,0,0,0,0,0,0,0,],
-            [1,0,1,0,1,0,1,0],
-            [0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,0,1,0]
-        ]
-
-    def printBoard(self) -> None:
-        '''
-        Prints board to console
-        '''
-        map = {-1:'o',1:'x',0:'-'}
-        for row in self._board:
-            for col in range(len(row)):
-                print(map[row[col]], end = " ")
-            print()
-
-    def isValidMove(self, board:[list], move: tuple, currPlayer) -> bool:
-        '''
-        Checks if current board and move are valid
-        '''
-        pass
-
-    def makeMove(self, player: int, move: tuple) -> bool:
-        '''
-        Parameters:
-            player: -1 or 1
-            move: tuple for board placement
-        Return:
-            boolean:
-                true - move success
-                false - move failed, move could not be placed
-        '''
-        pass
-
-    def getAllPossibleMoves(self, currPlayer: int) -> list:
-        '''
-        Parameters:
-            currPlayer: -1 or 1
-        Return:
-            list of possible moves in tuple form
-        '''
-        pass
-
-    def hasWinner(self) -> int:
-        '''
-        Return:
-            boolean, -1 if player -1 is winner
-                      1 if player 1 is winner
-                      0 if no winner
-        '''
-        pass
-
-    def makeRandomMove(self, currPlayer) -> bool:
-        '''
-        Parameters:
-            currPlayer: -1 or 1
-        Returns boolean:
-            true - move success
-            false - move failed
-        '''
-        pass
-
-game = Checkers()
-game.printBoard()
+# First param = -1
+# Second param = 1
+game = Game(Checkers(), AI.RandomAI(Checkers(), -1), AI.RandomAI(Checkers(), 1))
