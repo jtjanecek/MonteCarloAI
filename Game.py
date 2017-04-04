2a29a6066ffc3a4dbe7893729fc64558d82c634f
jtjanecek <janecektyler@gmail.com>
2017-04-04 10:21:01 -0700

    Added checks to see if there was a tie or no possible move in game loop

--- Game.py
+++ Game.py
@@ -10,30 +10,38 @@ class Game():
 
     def gameLoop(self, game, player1, player2):
         self._currMove = -1
-        while(game.getWinner() == 0):
+        p1CanMove = True
+        p2CanMove = True
+        while((game.getWinner() == 0) and (p1CanMove or p2CanMove)):
             game.printBoard()
             #time.sleep(2)
             if self._currMove == -1:
-                self.doMove(game,player1, self._currMove)
+                p1CanMove = self.doMove(game,player1, self._currMove)
                 self._currMove = 1
 
             elif self._currMove == 1:
-                self.doMove(game,player2, self._currMove)
+                p2CanMove = self.doMove(game,player2, self._currMove)
                 self._currMove = -1
 
         game.printBoard()
         print(game.getWinner()," is the winner!")
+        self._winner = game.getWinner()
 
-    def doMove(self, game, player, cursor):
-        while flag:
+    def doMove(self, game, player, cursor) -> bool:
+        while True:
+            if game.getAllPossibleMoves(game.getBoard(),cursor) == []:
+                return False
             move = player.getMove(list(game.getBoard()))
             if game.isValidMove(game.getBoard(),move, cursor):
                 game.makeMove(cursor, move)
-                break
+                return True
             else:
                 print("INVALID MOVE!")
 
 # First param = -1
 # Second param = 1
+d = {-1:0, 1:0, 0:0}
 for i in range(100):
     game = Game(Checkers(), AI.RandomAI(Checkers(), -1), AI.RandomAI(Checkers(), 1))
+    d[game._winner] += 1
+print(d)
