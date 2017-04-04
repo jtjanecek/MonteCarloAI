b2600f993592a8340b3da441829479dfc5f10143
jtjanecek <janecektyler@gmail.com>
2017-04-03 22:27:40 -0700

    Added console ai input

--- AI.py
+++ AI.py
@@ -14,9 +14,20 @@ class AI():
 class RandomAI(AI):
     def getMove(self, gameBoard) -> tuple:
         # l = self._game.getAllPossibleMoves(gameBoard, self._player)
-        # return l[int(random.random()*len(l))]
+        # return l[int(random.random())*len(l)]
+
+        l = self._game.getAllPossibleMoves(gameBoard, self._player)
+        r = int(random.random() * len(l))
+        print("R      =", r)
+        print("len(l) =", len(l))
+        print(l)
+        return l[r]
+
+
+class ConsoleAI(AI):
+    def getMove(self, gameBoard) -> tuple:
         f1 = input("F1: ")
         f2 = input("F2: ")
         t1 = input("T1: ")
         t2 = input("T2: ")
-        return ((f1,f2),(t1,t2))
+        return ((int(f1) - 1,int(f2) - 1),(int(t1) - 1,int(t2) - 1))
