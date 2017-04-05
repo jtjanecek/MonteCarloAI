6be779e5047620133ca1b1301770b3daeebc331b
jtjanecek <janecektyler@gmail.com>
2017-04-04 23:49:49 -0700

    Added monte carlo AI

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import AI
 from Checkers import Checkers
 import time
+from copy import deepcopy
 # Abstract class
 class Game():
     def __init__(self, game, player1, player2):
@@ -9,11 +10,15 @@ class Game():
         self.gameLoop(game, player1, player2)
 
     def gameLoop(self, game, player1, player2):
+        # player1 = -1
+        # player2 = 1
+
         self._currMove = -1
         p1CanMove = True
         p2CanMove = True
         while((game.getWinner() == 0) and (p1CanMove or p2CanMove)):
-            game.printBoard()
+            #game.printBoard()
+            print(".", end = "")
             #time.sleep(2)
             if self._currMove == -1:
                 p1CanMove = self.doMove(game,player1, self._currMove)
@@ -23,15 +28,14 @@ class Game():
                 p2CanMove = self.doMove(game,player2, self._currMove)
                 self._currMove = -1
 
-        game.printBoard()
-        print(game.getWinner()," is the winner!")
+        #game.printBoard()
         self._winner = game.getWinner()
 
     def doMove(self, game, player, cursor) -> bool:
         while True:
             if game.getAllPossibleMoves(game.getBoard(),cursor) == []:
                 return False
-            move = player.getMove(list(game.getBoard()))
+            move = player.getMove(deepcopy(game.getBoard()))
             if game.isValidMove(game.getBoard(),move, cursor):
                 game.makeMove(cursor, move)
                 return True
@@ -41,7 +45,16 @@ class Game():
 # First param = -1
 # Second param = 1
 d = {-1:0, 1:0, 0:0}
+r = AI.RandomAI(Checkers(), 1)
+#m = AI.RandomAI(Checkers(), 1)
+m = AI.MonteCarloAI(Checkers(), -1)
 for i in range(100):
-    game = Game(Checkers(), AI.RandomAI(Checkers(), -1), AI.RandomAI(Checkers(), 1))
+    print("Playing game:",i+1)
+    game = Game(Checkers(), m, r)
+    if game._winner == 1:
+       m.backPropagate(True)
+    else:
+       m.backPropagate(False)
+    print("                 Winner:",game._winner)
     d[game._winner] += 1
 print(d)
